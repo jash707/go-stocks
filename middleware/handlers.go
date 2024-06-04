16e62f948a575505b2cce7492d21d979e6cb4998
jash707 <jtamakuwala@tandemdiabetes.com>
2024-06-04 19:22:29 +0530

    Implemented handlers

--- middleware/handlers.go
+++ middleware/handlers.go
@@ -1,1 +1,88 @@
 package middleware
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"encoding/json"
+	"net/http"
+
+	"github.com/joho/godotenv"
+)
+
+type response struct {
+	ID      int64  `json:"id,omitempty"`
+	Message string `json:"message,omitempty"`
+}
+
+func CreateConnection() *sql.DB {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	db, err := sql.Open("postgres", os.Getenv("POSTGRES_URL"))
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected to postgres")
+
+	return db
+}
+
+
+func CreateStock(w http.ResponseWriter,r *http.Request){
+	var stock models.Stock
+
+	err := json.NewDecoder(r.Body).Decode(&stock)
+
+	if err != nil {
+		log.Fatal("Unable to decode the request body. %v",err)
+	}
+
+	insertID := insertStock(stock)
+
+	res := response{
+		ID: insertID,
+		Message: "Stock created successfully",
+	}
+
+	json.NewEncoder(w).Encode(res)
+}
+
+func GetStock(w http.ResponseWriter,r *http.Request){
+	params := mux.var (r)
+
+	id, err := strconv.Atoi(params["id"])
+
+	if err != nil {
+		log.Fatalf("Unable to convert the string into int. %v",err)
+	}
+
+	stock, err := getStock(int64(id))
+
+	if err != nil {
+		log.Fatalf("Unable to get stock. %v",err)
+	}
+}
+
+func GetAllStock(){
+	
+}
+
+func UpdateStock(){
+	
+}
+
+func DeleteStock(){
+	
+}
