3b095aed7312ced5af6b8ddedea2da7285cf0f07
jash707 <jtamakuwala@tandemdiabetes.com>
2024-06-11 17:17:44 +0530

    Implemented delete and update handlers

--- middleware/handlers.go
+++ middleware/handlers.go
@@ -2,11 +2,15 @@ package middleware
 
 import (
 	"database/sql"
-	"log"
-	"os"
 	"encoding/json"
+	"fmt"
+	"go-postgres-stocks/models"
+	"log"
 	"net/http"
+	"os"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
 
@@ -39,50 +43,97 @@ func CreateConnection() *sql.DB {
 	return db
 }
 
-
-func CreateStock(w http.ResponseWriter,r *http.Request){
+func CreateStock(w http.ResponseWriter, r *http.Request) {
 	var stock models.Stock
 
 	err := json.NewDecoder(r.Body).Decode(&stock)
 
 	if err != nil {
-		log.Fatal("Unable to decode the request body. %v",err)
+		log.Fatal("Unable to decode the request body. %v", err)
 	}
 
 	insertID := insertStock(stock)
 
 	res := response{
-		ID: insertID,
+		ID:      insertID,
 		Message: "Stock created successfully",
 	}
 
 	json.NewEncoder(w).Encode(res)
 }
 
-func GetStock(w http.ResponseWriter,r *http.Request){
-	params := mux.var (r)
+func GetStock(w http.ResponseWriter, r *http.Request) {
+	params := mux.vars(r)
 
 	id, err := strconv.Atoi(params["id"])
 
 	if err != nil {
-		log.Fatalf("Unable to convert the string into int. %v",err)
+		log.Fatalf("Unable to convert the string into int. %v", err)
 	}
 
 	stock, err := getStock(int64(id))
 
 	if err != nil {
-		log.Fatalf("Unable to get stock. %v",err)
+		log.Fatalf("Unable to get stock. %v", err)
 	}
+
+	json.NewEncoder(w).Encode(stock)
 }
 
-func GetAllStock(){
-	
+func GetAllStock(w http.ResponseWriter, r *http.Request) {
+	stocks, err := getAllStock()
+
+	if err != nil {
+		log.Fatalf("Unable to get all the stocks %v", err)
+	}
+
+	json.NewEncoder(w).Encode(stocks)
+
 }
 
-func UpdateStock(){
-	
+func UpdateStock(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	id, err := strconv.Atoi(params["id"])
+
+	if err != nil {
+		log.Fatalf("Unable to convert string to int. %v", err)
+	}
+
+	var stock models.Stock
+
+	err = json.NewDecoder(r.Body).Decode(&stock)
+
+	if err != nil {
+		log.Fatalf("Unable to decode to request body. %v", err)
+	}
+
+	updatedRows := updateStock(int64(id), stock)
+
+	msg := fmt.Sprintf("Stock updated seccessfully. Total rows/records affected %v", updatedRows)
+	res := response{
+		ID:      int64(id),
+		Message: msg,
+	}
+
+	json.NewEncoder(w).Encode(res)
 }
 
-func DeleteStock(){
-	
+func DeleteStock(w http.ResponseWriter, r *http.Request) {
+
+	params := mux.Vars(r)
+	id, err := strconv.ParseInt(params["id"])
+	if err != nil {
+		log.Fatalf("Unable to convert string to int. %v", err)
+	}
+
+	deletedRows := deleteStock(int64(id))
+
+	msg := fmt.Sprintf("Stock deleted successfully. Total rows/records %v", deletedRows)
+
+	res := response{
+		ID:      int64(id),
+		Message: msg,
+	}
+	json.NewEncoder(w).Encode(res)
 }
